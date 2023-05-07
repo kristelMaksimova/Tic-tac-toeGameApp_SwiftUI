1646ad3f5878a505a0b11eef690e0e8b4aa85bb3
Kristel <idmaksimovak@gmail.com>
2023-05-07 17:19:43 +0300

    Initial Commit

---
+++ Tic-tac-toeGameApp_SwiftUI/Tic_tac_toeGameApp_SwiftUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  Tic_tac_toeGameApp_SwiftUIApp.swift
+//  Tic-tac-toeGameApp_SwiftUI
+//
+//  Created by Kristi on 07.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Tic_tac_toeGameApp_SwiftUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
